dde29972a8f7d2ace910260790d70ee74854e9a6
陈炎东 <2915514915@qq.com>
2025-05-29 05:31:20 +0800

    ccc

---
+++ results/sacred/_sources/main_5329a14a9b3590d0926999e00f5a47c0.py
@@ -0,0 +1,222 @@
+import numpy as np
+import os
+import collections
+from os.path import dirname, abspath
+from copy import deepcopy
+from sacred import Experiment, SETTINGS
+from sacred.observers import FileStorageObserver
+from sacred.utils import apply_backspaces_and_linefeeds
+import sys
+import torch as th
+from utils.logging import get_logger
+import yaml
+
+# import run program
+from run import run as run
+from meta_train_run import run as meta_train_run
+from meta_test_run import run as meta_test_run
+from odis_train_run import run as odis_train_run
+from updet_m_train_run import run as updet_m_train_run
+from updet_l_train_run import run as updet_l_train_run
+from meta_distral_train_run import run as xdistral_train_run
+from original_distral_run import run as original_distral_run
+from new_task_repre_run import run as new_task_repre_run
+
+
+# 将sacred的日志输出到文件中，而不是输出到控制台
+SETTINGS['CAPTURE_MODE'] = "fd"  # set to "no" if you want to see stdout/stderr in console
+logger = get_logger()
+
+ex = Experiment("pymarl")
+ex.logger = logger
+ex.captured_out_filter = apply_backspaces_and_linefeeds  # 过滤掉日志中的回车和退格符号
+
+results_path = os.path.join(dirname(dirname(abspath(__file__))), "results")  # 日志保存路径
+
+
+@ex.main  # 标记这个函数为主要运行函数,Sacred会调用这个函数来运行实验
+# 设置随机数种子,并根据配置中的meta_train和meta_test字段来调用不同的运行函数
+def my_main(_run, _config, _log):
+    config = config_copy(_config)  # 深拷贝配置字典_config,避免修改外部传入的配置
+    np.random.seed(config["seed"])
+    th.manual_seed(config["seed"])  # th是pytorch的别名。th.manual_seed会在pytorch的随机数生成器中设置一个种子。
+    config['env_args']['seed'] = config["seed"]
+    if config.get("meta_train", False):  # 检查配置中是否有meta_train字段,如果没有则默认为False
+        if config.get("name") == "xtrans_train":
+            meta_train_run(_run, config, _log)  # 如果meta_train=True,则调用meta_train_run函数运行meta训练逻辑
+        elif config.get("name") == "updet_m_train":
+            updet_m_train_run(_run, config, _log)
+        elif config.get("name") == "updet_l_train":
+            updet_l_train_run(_run, config, _log)
+        elif config.get("name") == "odis_train":
+            odis_train_run(_run, config, _log)
+        elif config.get("name") == "xdistral_train":
+            xdistral_train_run(_run, config, _log)
+        elif config.get("name") == "original_distral":
+            original_distral_run(_run, config, _log)
+        elif config.get("name") == "xdistral_weight_train":
+            xdistral_train_run(_run, config, _log)
+        elif config.get("name") == "new_task_repre_train":
+            new_task_repre_run(_run, config, _log)
+    elif config.get("meta_test", False):  # 检查配置中是否有meta_test字段,如果没有则默认为False
+        meta_test_run(_run, config, _log)  # 如果meta_test=True,则调用meta_test_run函数运行meta测试逻辑
+
+    else:
+        run(_run, config, _log)  # 如果meta_train和meta_test都不是True,则调用默认的run函数运行常规逻辑
+
+
+"""
+params: 函数接收的参数列表,包含命令行传入的参数
+arg_name: 需要检查的参数名,如"--config"
+subfolder: 配置文件所在的子文件夹,如"algs"
+"""
+
+
+def _get_config(params, arg_name, subfolder):  # 从命令行参数params中获取指定名称arg_name的配置文件,加载并返回配置字典。
+    config_name = None  # 初始化配置文件名为空
+    for _i, _v in enumerate(params):  # 遍历命令行参数列表params
+        if _v.split("=")[0] == arg_name:  # 如果当前参数项的名称是arg_name
+            config_name = _v.split("=")[1]  # 则提取参数值作为配置文件名config_name
+            # 并从params中删除这个已经读取的参数
+            del params[_i]
+            break
+    # 如果config_name不为空,加载配置文件
+    if config_name is not None:
+        with open(os.path.join(os.path.dirname(__file__), "config", subfolder, "{}.yaml".format(config_name)),
+                  "r") as f:
+            try:
+                config_dict = yaml.safe_load(f)  # 读取并解析YAML文件为配置字典
+            except yaml.YAMLError as exc:
+                assert False, "{}.yaml error: {}".format(config_name, exc)
+        return config_dict
+
+
+def _get_env_config(env_name):  # 加载指定名称env_name的环境配置文件,并返回配置字典
+    with open(os.path.join(os.path.dirname(__file__), "config", "envs", "{}.yaml".format(env_name)), "r") as f:
+        try:
+            config_dict = yaml.safe_load(f)  # 读取并解析YAML文件为配置字典
+        except yaml.YAMLError as exc:
+            assert False, "{}.yaml error: {}".format(env_name, exc)
+    return config_dict
+
+
+def recursive_dict_update(d, u):  # 递归地更新字典d,使用字典u的数据
+    if u is not None:  # 只有当 u 不是 None 时才继续
+        for k, v in u.items():
+            if isinstance(v, collections.Mapping):  # 如果v是一个映射对象，比如字典
+                d[k] = recursive_dict_update(d.get(k, {}), v)  # 递归调用recursive_dict_update,更新键k对应的嵌套字典
+            else:
+                d[k] = v
+    return d
+
+
+"""
+config_copy函数的作用是深拷贝配置字典config,避免修改外部传入的配置。
+它的代码逻辑是:
+    判断config是字典还是列表
+    如果是字典:
+        对字典的每一项key-value递归调用config_copy,得到拷贝后的value
+        使用拷贝后的key-value构造一个新的字典并返回
+    如果是列表:
+        对列表的每一项递归调用config_copy,得到拷贝后的item
+        使用拷贝后的item构造一个新的列表并返回
+        否则,直接深拷贝config并返回
+"""
+
+
+def config_copy(config):
+    if isinstance(config, dict):
+        return {k: config_copy(v) for k, v in config.items()}
+    elif isinstance(config, list):
+        return [config_copy(v) for v in config]
+    else:
+        return deepcopy(config)
+
+
+if __name__ == '__main__':
+    params = deepcopy(sys.argv)  # 深拷贝命令行参数列表,避免修改外部传入的参数
+    # Get the defaults from default.yaml
+    with open(os.path.join(os.path.dirname(__file__), "config", "default.yaml"), "r") as f:
+        try:
+            config_dict = yaml.safe_load(f)
+        except yaml.YAMLError as exc:
+            assert False, "default.yaml error: {}".format(exc)
+
+    # Load algorithm base configs
+    alg_config = _get_config(params, "--config", "algs")  # 从命令行参数params中获取指定名称"--config"的配置文件,加载并返回配置字典。
+    # config_dict = {**config_dict, **alg_config}
+    config_dict = recursive_dict_update(config_dict, alg_config)  # 递归地更新字典config_dict,使用字典alg_config的数据
+
+    # check whether is meta_train
+    meta_train = config_dict.get("meta_train", False)  # 从配置字典config_dict中获取meta_train字段,如果没有则默认为False
+    meta_test = config_dict.get("meta_test", False)  # 从配置字典config_dict中获取meta_test字段,如果没有则默认为False
+
+    if meta_train:
+        task_config = _get_config(params, "--task-config",
+                                  "tasks")  # 从命令行参数params中获取指定名称"--task-config"的配置文件,加载并返回配置字典。
+        config_dict = recursive_dict_update(config_dict, task_config)  # 递归地更新字典config_dict,使用字典task_config的数据
+        env_name = config_dict["env"]
+        env_config = _get_env_config(env_name)  # 加载指定名称env_name的环境配置文件,并返回配置字典
+        config_dict = recursive_dict_update(config_dict, env_config)
+    elif meta_test:
+        task_config = _get_config(params, "--task-config", "tasks")
+        config_dict = recursive_dict_update(config_dict, task_config)
+        env_name = config_dict["env"]
+        env_config = _get_env_config(env_name)
+        config_dict = recursive_dict_update(config_dict, env_config)
+        if env_name == "sc2":
+            config_dict["env_args"]["map_name"] = config_dict[
+                "test_task"]  # 如果是sc2环境,则将配置字典config_dict中的map_name字段设置为test_task字段
+        else:
+            config_dict["env_args"]["task_id"] = config_dict[
+                "test_task"]  # 否则,将配置字典config_dict中的task_id字段设置为test_task字段
+    else:
+        # 如果既非meta_train也非meta_test，直接加载env_config,递归合并
+        env_config = _get_config(params, "--env-config", "envs")  #
+        config_dict = recursive_dict_update(config_dict, env_config)
+
+
+    # get config from argv, such as "remark"
+    def _get_argv_config(params):  # 从命令行参数params中提取参数配置
+        config = {}  # 初始化config字典用于保存提取的配置
+        to_del = []  # 初始化to_del列表,用于记录需要删除的参数
+        for _i, _v in enumerate(params):
+            item = _v.split("=")[0]  # 获取参数名称
+            if item[:2] == "--" and item not in ["envs", "algs"]:  # 如果参数名以"--"开头,且不是"envs"和"algs"
+                config_v = _v.split("=")[1]
+                try:
+                    config_v = eval(config_v)  # 将字符串config_v转换为字典或列表等对象
+                except:
+                    pass
+                config[item[2:]] = config_v  # 将参数名去掉"--"后作为键,参数值作为值,添加到config字典中
+                to_del.append(_v)  # 将参数_v添加到to_del列表中
+        for _v in to_del:
+            params.remove(_v)  # 从params中删除to_del列表中的参数
+        return config
+
+
+    config_dict = recursive_dict_update(config_dict, _get_argv_config(params))  # 将从命令行参数params中提取的参数配置,递归合并到全局配置
+
+    # if set map_name, we overwrite it
+    if "map_name" in config_dict:
+        assert not meta_train and meta_test, "Unexpected scenario!!!"  # 断言meta_train和meta_test都不为True
+        config_dict["env_args"]["map_name"] = config_dict["map_name"]
+
+    if "task_id" in config_dict:
+        assert not meta_train and meta_test, "Unexpected scenario!!!"  # 断言meta_train和meta_test都不为True
+        config_dict["env_args"]["task_id"] = config_dict["task_id"]
+
+    # 单任务测试特典
+    single_trans = config_dict.get("trans_phase", False)  # 从配置字典config_dict中获取trans_phase字段,如果没有则默认为False
+
+    if single_trans:
+        config_dict['epsilon_start'] = 0.05
+
+        # now add all the config to sacred
+    ex.add_config(config_dict)  # 将配置字典config_dict添加到sacred实验中
+
+    # Save to disk by default for sacred
+    logger.info("Saving to FileStorageObserver in results/sacred.")
+    file_obs_path = os.path.join(results_path, "sacred")  # 日志保存路径
+    ex.observers.append(FileStorageObserver.create(file_obs_path)) # 将日志保存到指定路径
+    ex.run_commandline(params) # 运行实验
---
+++ results/sacred/_sources/odis_train_run_410ad3e1c40af1a0468e449bec126360.py
@@ -0,0 +1,331 @@
+import datetime
+import os
+import pprint
+import time
+import threading
+import torch as th
+from types import SimpleNamespace as SN
+from utils.logging import Logger
+from utils.timehelper import time_left, time_str
+from os.path import dirname, abspath
+import copy
+import json
+
+from learners import REGISTRY as le_REGISTRY
+from runners import REGISTRY as r_REGISTRY
+from controllers import REGISTRY as mac_REGISTRY
+from components.episode_buffer import ReplayBuffer
+from components.transforms import OneHot
+
+import numpy as np
+
+
+def run(_run, _config, _log):
+
+    # check args sanity
+    _config = args_sanity_check(_config, _log)
+
+    args = SN(**_config)
+    args.device = "cuda" if args.use_cuda else "cpu"
+
+    # setup loggers
+    logger = Logger(_log)
+
+    _log.info("Experiment Parameters:")
+    experiment_params = pprint.pformat(_config,
+                                       indent=4,
+                                       width=1)
+    _log.info("\n\n" + experiment_params + "\n")
+
+    remark_str = getattr(args, "remark", "nop")
+    unique_token = "{}__{}_{}".format(args.name, remark_str, datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+    args.unique_token = unique_token
+    
+    training_pop_name = "-".join(args.train_tasks)
+    if args.use_tensorboard and not args.evaluate:
+        # only log tensorboard when in training mode
+        # though we are always in training mode when we reach here
+        '''这里是我修改的，以便区分debug和正常跑的文件'''
+        if _config.get("debug", False):
+            tb_logs_direc = os.path.join(dirname(dirname(abspath(__file__))), "results", "meta_train_debug", args.task, training_pop_name, args.name)
+        else:
+            tb_logs_direc = os.path.join(dirname(dirname(abspath(__file__))), "results", "meta_train", args.task, training_pop_name, args.name)
+
+
+        if not os.path.exists(tb_logs_direc):
+            os.makedirs(tb_logs_direc)
+
+        tb_exp_direc = os.path.join(tb_logs_direc, "{}").format(unique_token)
+        logger.setup_tb(tb_exp_direc)
+
+        # write config file
+        config_str = json.dumps(vars(args), indent=4)
+        with open(os.path.join(tb_exp_direc, "config.json"), "w") as f:
+            f.write(config_str)
+
+    # get unique output file name
+    # output_dirname = os.path.join(dirname(dirname(abspath(__file__))), "outputs", "meta_train", args.task, training_pop_name, args.name)
+    if _config.get("debug", False):
+        output_dirname = os.path.join(dirname(dirname(abspath(__file__))), "outputs", "meta_train_debug", args.task, training_pop_name, args.name)
+    else:
+        output_dirname = os.path.join(dirname(dirname(abspath(__file__))), "outputs", "meta_train", args.task, training_pop_name, args.name)
+
+    os.makedirs(output_dirname, exist_ok=True)
+
+    # set output dir
+    args.output_dir = os.path.join(output_dirname, unique_token)
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    output_file = os.path.join(output_dirname, f"{unique_token}.out")
+
+    # set model save dir
+    # args.save_dir = os.path.join(dirname(dirname(abspath(__file__))), "results", "meta_train", args.task, training_pop_name, args.name, "models", unique_token)
+    if _config.get("debug", False):
+        args.save_dir = os.path.join(dirname(dirname(abspath(__file__))), "results", "meta_train_debug", args.task, training_pop_name, args.name, "models", unique_token)
+    else:
+        args.save_dir = os.path.join(dirname(dirname(abspath(__file__))), "results", "meta_train", args.task, training_pop_name, args.name, "models", unique_token)
+
+    # sacred is on by default
+    logger.setup_sacred(_run, output_file)
+
+    # Run and train
+    run_sequential(args=args, logger=logger)
+
+    # Clean up after finishing
+    print("Exiting Main")
+
+    print("Stopping all threads")
+    for t in threading.enumerate():
+        if t.name != "MainThread":
+            print("Thread {} is alive! Is daemon: {}".format(t.name, t.daemon))
+            t.join(timeout=1)
+            print("Thread joined")
+
+    print("Exiting script")
+
+    # Making sure framework really exits
+    os._exit(os.EX_OK)
+
+
+def evaluate_sequential(args, runner):
+
+    for _ in range(args.test_nepisode):
+        runner.run(test_mode=True, evaluate_mode=True)
+
+    if args.save_replay:
+        runner.save_replay()
+    
+    runner.close_env()
+
+def run_sequential(args, logger):
+    # Init runner so we can get env info
+    train_tasks = args.train_tasks
+    args.n_tasks = len(args.train_tasks)  # number of tasks
+    # define main_args
+    main_args = copy.deepcopy(args)
+    task2args, task2runner, task2buffer = {}, {}, {}
+    task2scheme, task2groups, task2preprocess = {}, {}, {}
+    #  为每个task创建独立的args,runner,buffer
+    for task in args.train_tasks:
+        # define task_args
+        task_args = copy.deepcopy(args)
+        if task_args.env == "sc2":
+            task_args.env_args["map_name"] = task
+        elif task_args.env == "grid_mpe":
+            task_args.env_args["task_id"] = task
+        # 承接所有参数，确定runner
+        task2args[task] = task_args
+        task_runner = r_REGISTRY[args.runner](args=task_args, logger=logger, task=task)
+        task2runner[task] = task_runner
+        
+        # Set up schemes and groups here
+        env_info = task_runner.get_env_info()
+        task_args.n_agents = env_info["n_agents"]
+        task_args.n_actions = env_info["n_actions"]
+        task_args.state_shape = env_info["state_shape"]
+        
+        # Default/Base scheme
+        scheme = {
+            "state": {"vshape": env_info["state_shape"]},
+            "obs": {"vshape": env_info["obs_shape"], "group": "agents"},
+            "actions": {"vshape": (1,), "group": "agents", "dtype": th.long},
+            "avail_actions": {"vshape": (env_info["n_actions"],), "group": "agents", "dtype": th.int},
+            "reward": {"vshape": (1,)},
+            "terminated": {"vshape": (1,), "dtype": th.uint8},
+        }
+        groups = {
+            "agents": task_args.n_agents
+        }
+        preprocess = {
+            "actions": ("actions_onehot", [OneHot(out_dim=task_args.n_actions)])
+        }
+
+        task_buffer = ReplayBuffer(scheme, groups, task_args.buffer_size, env_info["episode_limit"] + 1,
+                            preprocess=preprocess,
+                            device="cpu" if task_args.buffer_cpu_only else task_args.device)
+        task2buffer[task] = task_buffer
+        
+        # store task information
+        task2scheme[task], task2groups[task], task2preprocess[task] = scheme, groups, preprocess
+
+    # get buffer.scheme for each task
+    task2buffer_scheme = {
+        task: task2buffer[task].scheme for task in train_tasks
+    }
+
+    # 所有任务共用一个mac
+    mac = mac_REGISTRY[main_args.mac](train_tasks, task2buffer_scheme, task2args, main_args)
+
+    # setup runner
+    for task in train_tasks:
+        # setup runner
+        task2runner[task].setup(scheme=task2scheme[task], groups=task2groups[task], preprocess=task2preprocess[task], mac=mac)
+
+    # 所有任务共用一个learner
+    learner = le_REGISTRY[main_args.learner](mac, logger, main_args)
+
+    if main_args.use_cuda:
+        learner.cuda()
+    
+    if main_args.checkpoint_path != "":
+        raise Exception("We don't support checkpoint loading in multi-task learning currently!")
+        
+    ########## start training ##########
+    episode = 0
+    model_save_time = 0  # 记录模型保存时间
+    # define training information for each task
+    task2train_info = {task: {} for task in train_tasks}
+    for task in train_tasks:
+        task2train_info[task]["last_test_T"] = -task2args[task].test_interval - 1
+        task2train_info[task]["last_log_T"] = 0
+        task2train_info[task]["start_time"] = time.time()
+        task2train_info[task]["last_time"] = task2train_info[task]["start_time"]
+
+    logger.console_logger.info("Beginning multi-task training with {} timesteps for each task".format(main_args.t_max))
+
+    # normal marl algorithm, e.g. QMIX, should not have pretrain phase
+    task2pretrain_phase = {task: getattr(task2args[task], "pretrain", False) for task in task2args}  # 记录每个任务是否处于pretrain阶段
+
+    task2terminated = {task: False for task in train_tasks}  # 记录每个任务是否训练结束
+    surrogate_task = np.random.choice(train_tasks)  # 用于保存模型的任务
+    
+    # get some common information
+    batch_size_train = main_args.batch_size
+    batch_size_run = main_args.batch_size_run
+    while True:
+        if all(task2terminated.values()):
+            # if all task learning terminated, jump out
+            break
+        # 每次训练前打乱任务顺序
+        np.random.shuffle(train_tasks)
+        # train each task
+        for task in train_tasks:
+            if task2terminated[task]:
+                continue
+            # Run for a whole episode at a time
+            # 这里疑似没有输入pre_train参数,已做修改，出问题记得调回来
+            episode_batch = task2runner[task].run(test_mode=False,pretrain_phase=task2pretrain_phase[task])
+            # 遍历每个任务,运行一个episode,把生成的episode数据插入replay buffer。
+            task2buffer[task].insert_episode_batch(episode_batch)
+            # 如果buffer中样本数量足够,则开始训练:
+            if task2buffer[task].can_sample(batch_size_train):
+                # balance between paralle and episode run
+                terminated = False
+                for _run in range(batch_size_run):
+                    episode_sample = task2buffer[task].sample(batch_size_train) # 从buffer中采样batchsize个样本,送入learner训练模型。
+
+                    # Truncate batch to only filled timesteps
+                    # 首先计算episode_sample中的有效时间步数max_ep_t,并截断episode_sample,删除超过max_ep_t之后的无效内容。
+                    max_ep_t = episode_sample.max_t_filled()
+                    episode_sample = episode_sample[:, :max_ep_t]
+                
+                    if episode_sample.device != task2args[task].device:
+                        episode_sample.to(task2args[task].device)
+
+                    "修改开始"
+                    # if task2args[task].pretrain:
+                    #     if hasattr(learner, 'pretrain'):
+                    #         terminated = learner.pretrain(episode_sample, task2runner[task].t_env, episode, task)
+                    #     else:
+                    #         raise ValueError("Do pretraining with a learner that does not have a `pretrain` method!")
+                    # else:
+                    terminated = learner.train(episode_sample, task2runner[task].t_env, episode, task)
+                    #if terminated:
+                    #    break
+                    #if terminated is not None and terminated:
+                     #   break
+
+
+                "修改结束"
+                if terminated:
+                    # 检查当前task是否进行表征学习(only_repre_learning),如果是则直接标记task已结束。
+                    # if getattr(task2args[task], "only_repre_learning", False):
+                    #     task2terminated[task] = True
+                    #     continue
+                    # else:
+                        # 否则输出日志,表示预训练阶段结束,进入主训练阶段。
+                        logger.console_logger.info(f"[Task {task}] Finish pretrain and begin training for {task2args[task].t_max} timesteps")
+                        # Reset some properties in run.py, not need to modify episode, last_log_T, ...
+                        # 重置一些训练状态的变量:
+                        task2pretrain_phase[task] = False  # 将pretrain_phase标记为False,表示进入主训练阶段
+                        # 重置start_time和last_time,用于计算训练时间
+                        task2train_info[task]["start_time"] = time.time()
+                        task2train_info[task]["last_time"] = task2train_info[task]["start_time"]
+                        # Reset some properties about buffer and runner
+                        task2buffer[task].clear()  # 清空经验回放缓冲区buffer
+                        task2runner[task].t_env = 0  # 重置环境步数t_env为0
+                        continue
+            # 进入主训练阶段
+            if not task2pretrain_phase[task]:
+                # Execute test runs once in a while
+                n_test_runs = max(1, task2args[task].test_nepisode // task2runner[task].batch_size)  # 计算每次测试的episode数量
+                if (task2runner[task].t_env - task2train_info[task]["last_test_T"]) / task2args[task].test_interval >= 1.0:
+                    logger.console_logger.info("[Task {}] t_env: {} / {}".format(task, task2runner[task].t_env, task2args[task].t_max))
+                    logger.console_logger.info("[Task {}] Estimated time left: {}. Time passed: {}".format(
+                        task, time_left(task2train_info[task]["last_time"], task2train_info[task]["last_test_T"], task2runner[task].t_env, task2args[task].t_max), time_str(time.time() - task2train_info[task]["start_time"])))
+                    task2train_info[task]["last_time"] = time.time()
+                    
+                    task2train_info[task]["last_test_T"] = task2runner[task].t_env
+                    for _ in range(n_test_runs):
+                        task2runner[task].run(test_mode=True)
+
+                if main_args.save_model and task == surrogate_task and (task2runner[task].t_env - model_save_time >= main_args.save_model_interval or model_save_time == 0):
+                    model_save_time = task2runner[task].t_env
+                    # get model path
+                    save_path = os.path.join(main_args.save_dir, str(task2runner[task].t_env))
+                    # make dir
+                    os.makedirs(save_path, exist_ok=True)
+                    logger.console_logger.info("Saving models to {}".format(save_path))
+
+                    learner.save_models(save_path)
+
+                episode += batch_size_run
+
+                # check whether task terminated and close env
+                if task2runner[task].t_env > task2args[task].t_max:  # 如果当前环境步数t_env超过最大步数t_max,则标记task已结束
+                    task2terminated[task] = True
+                    # schedule surrogate task
+                    if task == surrogate_task and not all(task2terminated.values()):
+                        surrogate_task = np.random.choice([task for task in train_tasks if not task2terminated[task]])
+                        model_save_time = -1
+
+
+
+
+        # learner.update(pretrain=task2args[task].pretrain)
+
+
+def args_sanity_check(config, _log):
+
+    # set CUDA flags
+    # config["use_cuda"] = True # Use cuda whenever possible!
+    if config["use_cuda"] and not th.cuda.is_available():
+        config["use_cuda"] = False
+        _log.warning("CUDA flag use_cuda was switched OFF automatically because no CUDA devices are available!")
+
+    if config["test_nepisode"] < config["batch_size_run"]:
+        config["test_nepisode"] = config["batch_size_run"]
+    else:
+        config["test_nepisode"] = (config["test_nepisode"]//config["batch_size_run"]) * config["batch_size_run"]
+
+    return config
